719954cadfe31eeab94e646974b633e87056500e
Ferdinand Ratajczak <ferdinand@fratajcz.dev>
2022-06-05 16:42:14 +0200

    Fix / and pubg/ urls

--- pubg/urls.py
+++ pubg/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="rankings/")),
     path("rankings/", views.all_rankings_view, name="index"),
     path("player_list/", views.all_rankings_view, name="player_list"),
     path("search/", views.player_search, name="player_search"),
